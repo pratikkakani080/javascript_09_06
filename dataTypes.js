7ffa96b4ef64c7ca8fbe2ec77049b09172d5e506
Your Name <you@example.com>
2026-06-11 17:04:40 +0530

    add: variables + datatypes

---
+++ dataTypes.js
@@ -0,0 +1,35 @@
+// Data types
+
+// Premitive
+    // String
+        // '4444'  "      "  ``
+        //  4 + 4 + '4' = '84'
+    // Number
+        // 234234234 || 23234.4454 || -234234.234 || -2^53 to 2^53
+    // Null
+        // null
+    // Undefined
+        // undefined
+    // Boolean
+        // true || false
+    // BigInt
+        // 1n
+    // Symbol
+        // Symbol() === Symbol() = false
+
+// NOn-premitive / Complex
+    // Object
+      const person = {
+            name: 'Pratik',
+            age: 30,
+            address: {
+                city: 'Ahmedabad',
+                state: 'Gujarat'
+            },
+            height: 5.11,
+            isActive: true,
+            hobbies: ['coding', 'reading', 'gaming'] 
+        }
+    // Array
+        const numbers = [1, 2, 3, 4, 5];
+    // Function
---
+++ variables.js
@@ -0,0 +1,32 @@
+console.log("Hello, World! this is a javascript code");
+
+// let, var, const
+let name = "John";
+name = "Doe";
+console.log(name);
+
+var add = 'street'
+add = 'avenue'
+var add = 'sfsfsd'
+console.log(add);
+
+const age = 30;
+// age = 31; // This will cause an error
+// const age = 32
+console.log(age);
+
+// always use naming convention properly (based on data)
+
+// Casing
+// Camel Case
+let firstName = "John";
+
+// Lower case
+let lastname = 'doe'
+
+// Upper Case
+let ADDRESS = 'hirabaug'
+
+// snake case
+let last_name = ''
+
